64b1b828044dd8c4e82ada3eae898ec6bebbf0cd
Pedro Dalfior <dalfiorpedro697@gmail.com>
2026-04-24 13:34:48 -0300

    fix: reverter inicializacao Firebase e ignorar warnings no CI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/loading_screen.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const EcoBotApp());
 }
 
